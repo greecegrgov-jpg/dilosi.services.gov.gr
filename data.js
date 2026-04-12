5982ad0a6725c70a27424d85b104bc8c50bd7414
greecegrgov-jpg <greece.gr.gov@gmail.com>
2026-04-12 17:19:31 +0500

    Update data.js

--- data.js
+++ data.js
@@ -1,33 +1,161 @@
 const users = {
   "GOV001": {
-    name: "ANGELIKI",
-    surname: "GERANIUM",
-    father: "KOSMAS",
-    mother: "EVGENIA",
-    id: "GOV001",
-    dob: "14/07/1982",
-    tax: "127862703",
-    signatureDate: "21/09/2025 14:38:50",
-    issueDate: "21/09/2025 16:38:50",
-    verificationCode: "Hmp8KarPrCVf16DmL02HQ",
-    status: "The document was issued",
-    standard: "GNISIO-YP0GRAFIS",
-    certificate: "certificates/cert1.pdf"
-  },
-
-  "GOV002": {
     name: "ALI",
     surname: "KHAN",
     father: "AHMED KHAN",
     mother: "FATIMA",
-    id: "GOV002",
+    id: "GOV001",
     dob: "01/01/1995",
-    tax: "987654321",
+    tax: "123456789",
     signatureDate: "10/04/2026 10:20:30",
     issueDate: "10/04/2026 11:00:00",
-    verificationCode: "XyZ123VerifyCode002",
+    verificationCode: "VER-001-AK",
     status: "The document was issued",
     standard: "GNISIO-YP0GRAFIS",
-    certificate: "certificates/cert1.pdf"
+    certificate: "cert1.pdf"
+  },
+
+  "GOV002": {
+    name: "USMAN",
+    surname: "ALI",
+    father: "RASHID ALI",
+    mother: "SHAHNAZ",
+    id: "GOV002",
+    dob: "05/03/1996",
+    tax: "223456789",
+    signatureDate: "11/04/2026 11:00:00",
+    issueDate: "11/04/2026 11:30:00",
+    verificationCode: "VER-002-UA",
+    status: "The document was issued",
+    standard: "GNISIO-YP0GRAFIS",
+    certificate: "cert2.pdf"
+  },
+
+  "GOV003": {
+    name: "HASSAN",
+    surname: "RAZA",
+    father: "RAZA ALI",
+    mother: "NAZIA",
+    id: "GOV003",
+    dob: "08/07/1994",
+    tax: "323456789",
+    signatureDate: "12/04/2026 09:00:00",
+    issueDate: "12/04/2026 09:30:00",
+    verificationCode: "VER-003-HR",
+    status: "The document was issued",
+    standard: "GNISIO-YP0GRAFIS",
+    certificate: "cert3.pdf"
+  },
+
+  "GOV004": {
+    name: "BILAL",
+    surname: "AHMED",
+    father: "TARIQ AHMED",
+    mother: "PARVEEN",
+    id: "GOV004",
+    dob: "10/10/1993",
+    tax: "423456789",
+    signatureDate: "13/04/2026 12:00:00",
+    issueDate: "13/04/2026 12:30:00",
+    verificationCode: "VER-004-BA",
+    status: "The document was issued",
+    standard: "GNISIO-YP0GRAFIS",
+    certificate: "cert4.pdf"
+  },
+
+  "GOV005": {
+    name: "ZAIN",
+    surname: "MALIK",
+    father: "IMRAN MALIK",
+    mother: "RUKHSANA",
+    id: "GOV005",
+    dob: "15/06/1997",
+    tax: "523456789",
+    signatureDate: "14/04/2026 14:00:00",
+    issueDate: "14/04/2026 14:30:00",
+    verificationCode: "VER-005-ZM",
+    status: "The document was issued",
+    standard: "GNISIO-YP0GRAFIS",
+    certificate: "cert5.pdf"
+  },
+
+  "GOV006": {
+    name: "AHSAN",
+    surname: "IQBAL",
+    father: "IQBAL HUSSAIN",
+    mother: "SAIMA",
+    id: "GOV006",
+    dob: "20/02/1992",
+    tax: "623456789",
+    signatureDate: "15/04/2026 15:00:00",
+    issueDate: "15/04/2026 15:30:00",
+    verificationCode: "VER-006-AI",
+    status: "The document was issued",
+    standard: "GNISIO-YP0GRAFIS",
+    certificate: "cert6.pdf"
+  },
+
+  "GOV007": {
+    name: "SAAD",
+    surname: "KHAN",
+    father: "YOUSAF KHAN",
+    mother: "SAMINA",
+    id: "GOV007",
+    dob: "18/09/1998",
+    tax: "723456789",
+    signatureDate: "16/04/2026 16:00:00",
+    issueDate: "16/04/2026 16:30:00",
+    verificationCode: "VER-007-SK",
+    status: "The document was issued",
+    standard: "GNISIO-YP0GRAFIS",
+    certificate: "cert7.pdf"
+  },
+
+  "GOV008": {
+    name: "HAMZA",
+    surname: "ALI",
+    father: "NAVEED ALI",
+    mother: "SHAZIA",
+    id: "GOV008",
+    dob: "25/12/1999",
+    tax: "823456789",
+    signatureDate: "17/04/2026 17:00:00",
+    issueDate: "17/04/2026 17:30:00",
+    verificationCode: "VER-008-HA",
+    status: "The document was issued",
+    standard: "GNISIO-YP0GRAFIS",
+    certificate: "cert8.pdf"
+  },
+
+  "GOV009": {
+    name: "OWAIS",
+    surname: "AHMED",
+    father: "JAVED AHMED",
+    mother: "NASREEN",
+    id: "GOV009",
+    dob: "03/05/1991",
+    tax: "923456789",
+    signatureDate: "18/04/2026 18:00:00",
+    issueDate: "18/04/2026 18:30:00",
+    verificationCode: "VER-009-OA",
+    status: "The document was issued",
+    standard: "GNISIO-YP0GRAFIS",
+    certificate: "cert9.pdf"
+  },
+
+  "GOV010": {
+    name: "FAHAD",
+    surname: "HUSSAIN",
+    father: "HUSSAIN ALI",
+    mother: "KANEZ",
+    id: "GOV010",
+    dob: "07/08/1990",
+    tax: "103456789",
+    signatureDate: "19/04/2026 19:00:00",
+    issueDate: "19/04/2026 19:30:00",
+    verificationCode: "VER-010-FH",
+    status: "The document was issued",
+    standard: "GNISIO-YP0GRAFIS",
+    certificate: "cert10.pdf"
   }
 };
