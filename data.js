8ae74e12de0f84fffb64ce8a31050c2ed03541b9
greecegrgov-jpg <greece.gr.gov@gmail.com>
2026-04-12 15:39:42 +0500

    Create data.js

---
+++ data.js
@@ -0,0 +1,15 @@
+const users = {
+  "GOV001": {
+    name: "Ali Khan",
+    father: "Ahmed Khan",
+    id: "GOV001",
+    certificate: "cert1.pdf"
+  },
+
+  "GOV002": {
+    name: "Usman Ali",
+    father: "Rashid Ali",
+    id: "GOV002",
+    certificate: "cert2.pdf"
+  }
+};
