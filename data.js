567b2d6dc0c458051dac7c1b3fe96f51a837be59
greecegrgov-jpg <greece.gr.gov@gmail.com>
2026-04-12 15:54:48 +0500

    Update data.js

--- data.js
+++ data.js
@@ -1,15 +1,33 @@
 const users = {
   "GOV001": {
-    name: "Ali Khan",
-    father: "Ahmed Khan",
+    name: "ANGELIKI",
+    surname: "GERANIUM",
+    father: "KOSMAS",
+    mother: "EVGENIA",
     id: "GOV001",
+    dob: "14/07/1982",
+    tax: "127862703",
+    signatureDate: "21/09/2025 14:38:50",
+    issueDate: "21/09/2025 16:38:50",
+    verificationCode: "Hmp8KarPrCVf16DmL02HQ",
+    status: "The document was issued",
+    standard: "GNISIO-YP0GRAFIS",
     certificate: "cert1.pdf"
   },
 
   "GOV002": {
-    name: "Usman Ali",
-    father: "Rashid Ali",
+    name: "ALI",
+    surname: "KHAN",
+    father: "AHMED KHAN",
+    mother: "FATIMA",
     id: "GOV002",
+    dob: "01/01/1995",
+    tax: "987654321",
+    signatureDate: "10/04/2026 10:20:30",
+    issueDate: "10/04/2026 11:00:00",
+    verificationCode: "XyZ123VerifyCode002",
+    status: "The document was issued",
+    standard: "GNISIO-YP0GRAFIS",
     certificate: "cert2.pdf"
   }
 };
